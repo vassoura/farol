bce7eaf8a2cb9245a78c5310f3fa373d39be2615
Ricardo Piekarski <ricardopiekarski@gmail.com>
2012-03-03 11:28:01 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farol::Application.config.secret_token = '95f03e4d4381dc958899e4c14165779ec4b20469b758db2ef6d2a3aba96fb2fe18fef844265a02f866c5c731191f180bb60db708c8a2c489f075b40b8be6bc66'
